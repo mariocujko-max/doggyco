015eaab6e33e3d209590bb5efd5a0b41ffdd4c91
mario-max <mariocujko@gmail.com>
2026-04-17 21:08:53 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'doggyco-cache-v2';
+
+// Installations-Event
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+// Aktivierungs-Event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Fetch-Event (für PWABuilder notwendig)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
